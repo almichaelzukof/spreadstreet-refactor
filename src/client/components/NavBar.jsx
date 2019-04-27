9faa307161fd319d915724dd5c2ce411dae306cb
John Young <john.young@spreadstreet.io>
2019-04-27 15:25:17 -0400

    login flow working...sweet

--- src/client/components/NavBar.jsx
+++ src/client/components/NavBar.jsx
@@ -7,22 +7,13 @@ const LogoutButton = () => (
   <button className='btn btn-green' onClick={() => onLogout() }>Log Out</button>
 );
 
-const handleLinks = (menuItems) => {
-  // if (loggedIn) {
-  //   const hidden = ['Sign Up', 'Login'];
-  //   return menuItems.filter((item) => !hidden.includes(item.name));
-  // }
-  const hidden = ['Sign Out'];
-  return menuItems.filter((item) => !hidden.includes(item.name));
-};
-
 function Navbar() {
   const [globalState] = useGlobal();
-  const {menuItems} = globalState;
+  const {menuItems, stitchUser} = globalState;
   return (
     <nav>
       <ul className='list-reset flex flex-row m-4'>
-        {handleLinks(menuItems).map((item) => <li className='p-4'>{item.name}</li>)}
+        {menuItems.map((item) => <li className='p-4'>{item.name}</li>)}
       </ul>
       <LogoutButton />
     </nav>
